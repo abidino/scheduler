666799e3775dc7eaadab855ab5bcb20140ea4440
abidino <abidinozd@gmail.com>
2023-05-08 16:18:03 +0300

    removeTask was added

--- src/main/java/dev/abidino/schedular/TaskServiceImpl.java
+++ src/main/java/dev/abidino/schedular/TaskServiceImpl.java
@@ -4,13 +4,29 @@ import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.TimeZone;
+import java.util.concurrent.ScheduledFuture;
 
 @Service
 public record TaskServiceImpl(TaskScheduler taskScheduler) implements TaskService {
+
+    private static Map<String,ScheduledFuture<?>> taskMap = new HashMap<>();
+
     @Override
-    public void schedule(TaskDefinition taskDefinition, Runnable runnable) {
+    public void addTask(TaskDefinition taskDefinition, Runnable runnable) {
         CronTrigger cronTrigger = new CronTrigger(taskDefinition.cronExpression(), TimeZone.getTimeZone(TimeZone.getDefault().getID()));
-        taskScheduler.schedule(runnable, cronTrigger);
+        ScheduledFuture<?> schedule = taskScheduler.schedule(runnable, cronTrigger);
+        taskMap.put(taskDefinition.id(), schedule);
+    }
+    @Override
+    public void removeTask(String taskId){
+        ScheduledFuture<?> scheduledFuture = taskMap.get(taskId);
+        if (Objects.nonNull(scheduledFuture)){
+            scheduledFuture.cancel(true);
+            taskMap.remove(taskId);
+        }
     }
 }
